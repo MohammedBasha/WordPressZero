c415b90c2ecae1c4ed32244fcf500d4648c818a3
MohammedBasha <me@mohammedbasha.com>
2020-04-19 22:36:17 +0200

    Adding the style file in the function.php file

--- wp-content/themes/wordpress101/functions.php
+++ wp-content/themes/wordpress101/functions.php
@@ -1,10 +1,54 @@
+<?php 
+
 /*
 ** Adding the custom style
 */
 
 function add_custom_style() {
 	wp_enqueue_style(
-		'custom-bs',
+		'bootstrap-css',
 		get_template_directory_uri() . '/css/bootstrap.min.css'
 	);
+
+	wp_enqueue_style(
+		'fontawesome-css',
+		get_template_directory_uri() . '/css/font-awesome.min.css'
+	);
+
+	wp_enqueue_style(
+		'main-css',
+		get_template_directory_uri() . '/css/main.css'
+	);
 }
+
+
+/*
+** Adding the custom script
+*/
+
+function add_custom_script() {
+	wp_enqueue_script(
+		'bootstrap-script',
+		get_template_directory_uri() . '/js/bootstrap.min.js',
+		array(),
+		false,
+		true
+	);
+
+	wp_enqueue_script(
+		'main-script',
+		get_template_directory_uri() . '/js/main.js',
+		array(),
+		false,
+		true
+	);
+}
+
+/*
+** Adding the Actions to trigger the styles and scripts functions
+*/
+
+add_action('wp_enqueue_scripts', 'add_custom_style');
+add_action('wp_enqueue_scripts', 'add_custom_script');
+
+?>
