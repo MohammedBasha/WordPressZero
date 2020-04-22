e26170c9dfd7115bf89a04f65ae5a5c7223c607c
MohammedBasha <me@mohammedbasha.com>
2020-04-22 14:36:30 +0200

    Adding a post structure

--- wp-content/themes/wordpress101/index.php
+++ wp-content/themes/wordpress101/index.php
@@ -2,6 +2,29 @@
 
 <?php get_header(); ?>
 
-<h1>This is the Main Content.</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="main-post">
+				<h3 class="post-title">POst Title</h3>
+				<div class="post-info">
+					<span class="post-author">Author</span>
+					<span class="post-date">Date</span>
+					<span class="post-comments">20 comments</span>
+				</div>
+				<a href="#" class="post-image">
+					<img src="//via.placeholder.com/350x100" alt="alt">
+				</a>
+				<p class="post-desc">
+					Content
+				</p>
+				<hr />
+				<p class="post-categories">
+					Categories
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
