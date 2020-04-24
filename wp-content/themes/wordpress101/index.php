44f3250ef20690a2c4e7f9d3c1d1d7bdd124b6d9
MohammedBasha <me@mohammedbasha.com>
2020-04-24 12:53:29 +0200

    Adding the post dynamic data through the wordpress functions

--- wp-content/themes/wordpress101/index.php
+++ wp-content/themes/wordpress101/index.php
@@ -2,28 +2,71 @@
 
 <?php get_header(); ?>
 
+
 <div class="container">
 	<div class="row">
-		<div class="col-sm-6">
-			<div class="main-post">
-				<h3 class="post-title">Post Title</h3>
-				<div class="post-info">
-					<span class="post-author">Author</span>
-					<span class="post-date">Date</span>
-					<span class="post-comments">20 comments</span>
-				</div>
-				<a href="#" class="post-image">
-					<img src="//via.placeholder.com/350x100" alt="alt">
-				</a>
-				<p class="post-desc">
-					Content
-				</p>
-				<hr />
-				<p class="post-categories">
-					Categories
-				</p>
-			</div>
-		</div>
+		<?php
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post();
+
+					?>
+					<div class="col-sm-6">
+						<div class="main-post">
+							<h3 class="post-title">
+								<a href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+								</a>
+							</h3>
+							<div class="post-info">
+								<span class="post-author">
+									<i class="fa fa-user" aria-hidden="true"></i>
+									<?php the_author_posts_link(); ?>
+								</span>
+								<span class="post-date">
+									<i class="fa fa-calendar" aria-hidden="true"></i>
+									<?php the_time('F j, Y'); ?>
+								</span>
+								<span class="post-comments">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+									<?php comments_popup_link(
+										'No comments',
+										'1 comment',
+										'% comments',
+										'comment-url',
+										'Comments disabled'
+									); ?>
+								</span>
+							</div>
+							<a href="<?php the_permalink(); ?>"
+								title="<?php the_title_attribute(); ?>"
+								class="post-image">
+								<?php the_post_thumbnail(
+									'',
+									[
+										'class' => 'img-responsive img-thumbnail'
+									]
+								); ?>
+							</a>
+							<p class="post-desc">
+								<?php
+									the_content('Continue reading ' . get_the_title());
+								?>
+							</p>
+							<hr />
+							<p class="post-categories">
+								<i class="fa fa-tags" aria-hidden="true"></i>
+								<?php the_category(', '); ?>
+							</p>
+						</div>
+					</div>
+
+					<?php
+				}
+			}
+		?>
+
+		
 	</div>
 </div>
 
