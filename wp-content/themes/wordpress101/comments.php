b756e5775d11b8fac459422af10dec75bb65b1f8
MohammedBasha <me@mohammedbasha.com>
2020-04-28 11:45:55 +0200

    Adding the Comment Form

--- wp-content/themes/wordpress101/comments.php
+++ wp-content/themes/wordpress101/comments.php
@@ -1,7 +1,14 @@
 <?php
 
-if (comments_open()) {
-	echo '<h3>' . comments_number('No comments', '1 Comment', '% comments') . '</h3>';
+if (comments_open()) { 
+
+	// Printing the comments bumber in h3 tag 
+	?>
+
+	<h5><?php comments_number('No comments', '1 Comment', '% comments') ?> </h5>
+
+	<?php
+	// printing the comments list with predefined bootstrap's classes
 	echo "<ul class='list-unstyled comments-list'>";
 	$comments_arguments = [
 		'max_depth' => 3,
@@ -12,6 +19,8 @@ if (comments_open()) {
 	wp_list_comments($comments_arguments);
 
 	echo "</ul>";
+
+	comment_form();
 } else {
 	echo 'Comments are disabled';
 }
