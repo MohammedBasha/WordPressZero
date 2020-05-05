72f83fddf8ad91f7c8c478631f50fc50fa37a0e3
MohammedBasha <me@mohammedbasha.com>
2020-05-06 00:19:02 +0200

    Adding a 404 page

---
+++ wp-content/themes/wordpress101/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="main-content page-404">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<p class="lead">You can try searching for that instead</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
