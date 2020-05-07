afcce41add7514c50af92aefe4765b2b5e35f44e
MohammedBasha <me@mohammedbasha.com>
2020-05-07 07:56:43 +0200

    Adding a custom sidebar template for the JavaScript category

---
+++ wp-content/themes/wordpress101/sidebar-js.php
@@ -0,0 +1,27 @@
+<?php ?>
+
+<div class="sidebar-wrapper sidebar-js">
+	<div class="widget">
+		<h3 class="widget-title"><?php single_cat_title(); ?> statistics</h3>
+		<div class="widget-content">
+			<ul>
+				<li>
+					<span>Comments count:</span>
+					<span>Articles count:</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="widget">
+		<h3 class="widget-title">widget title</h3>
+		<div class="widget-content">
+			widget-content
+		</div>
+	</div>
+	<div class="widget">
+		<h3 class="widget-title">widget title</h3>
+		<div class="widget-content">
+			widget-content
+		</div>
+	</div>
+</div>
